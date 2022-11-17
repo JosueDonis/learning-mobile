931ce6d0b56cc797bfb832b183a13448c7df28f9
Josh <brayan120agustin@gmail.com>
2022-11-16 22:29:25 -0600

    Feat: 👨‍�aadd icon title and source favicon

--- src/App.tsx
+++ src/App.tsx
@@ -15,25 +15,9 @@ import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
 
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
 
 /* Theme variables */
-import './theme/variables.css';
-
+import './theme/global.scss';
 setupIonicReact();
 
 const App: React.FC = () => (
